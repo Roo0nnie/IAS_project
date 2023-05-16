5dcb2c147068f709af964ad28d1873dcf18b7183
JMichaelVargas <johnmichaelelli.vargas@bicol-u.edu.ph>
2023-05-16 23:00:03 +0800

    add new features, fix responsive and design

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require("express")
 var app = express();
+const path = require("path");
 
 const http = require('http');
+const { count } = require("console");
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname+"/public")));
 
 const port = 8000;
 let socketsConected = new Set()
@@ -24,11 +28,18 @@ app.get("/login", function(req, res){
 
 io.on("connection",function(socket){
     socket.on("newuser",function(username){
-        socket.broadcast.emit("update", username + " joined the convo")
+      socket.username = username;
+      socket.broadcast.emit("update", username + " joined the convo")
+      socketsConected.add(socket.id)
+      console.log(socketsConected.size)
+      io.emit("countUser", socketsConected.size);
     });
     
     socket.on("exituser",function(username){
       socket.broadcast.emit("update", username + " left the convo")
+      socketsConected.delete(socket.id)
+      console.log(socketsConected.size)
+      io.emit("countUser", socketsConected.size);
     });
   
     socket.on('message', (data) => {
@@ -37,6 +48,24 @@ io.on("connection",function(socket){
   
     socket.on("chat",function(message){
       socket.broadcast.emit("chat", message);
+    });
+
+    socket.on("typing", function () {
+      socket.broadcast.emit("typing", socket.username);
+    });
+  
+    socket.on("stopTyping", function () {
+      socket.broadcast.emit("stopTyping", socket.username);
+    });
+
+    socket.on("disconnect", function () {
+      if (socket.username) {
+        const username = socket.username;
+        socket.broadcast.emit("update", username + " left the convo");
+        socketsConected.delete(socket.id)
+        console.log(socketsConected.size)
+        io.emit("countUser", socketsConected.size);
+      }
     });
   });
   
