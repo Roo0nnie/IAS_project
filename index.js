240cbf481efc98adf9f81569ef4d8311d38ffbd2
Ronnie Estillero <ronniewarrior09@gmail.com>
2024-03-09 19:11:02 +0800

    first commit

--- index.js
+++ index.js
@@ -1,76 +1,65 @@
-var express = require("express")
+var express = require("express");
 var app = express();
 const path = require("path");
 
-const http = require('http');
+const http = require("http");
 const { count } = require("console");
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 const port = 8000;
-let socketsConected = new Set()
+let socketsConected = new Set();
 
-const defURL = "http://localhost:8000/"
+const defURL = "http://localhost:8000/";
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/app", function(req, res){
-res.render("pages/app", {socketURL:defURL});
+app.get("/app", function (req, res) {
+  res.render("pages/app", { socketURL: defURL });
 });
 
-app.get("/login", function(req, res){
-    res.render("pages/login", {socketURL:defURL});
-});
+// app.get("/login", function (req, res) {
+//   res.render("pages/login", { socketURL: defURL });
+// });
 
-io.on("connection",function(socket){
-    socket.on("newuser",function(username){
-      socket.username = username;
-      socket.broadcast.emit("update", username + " joined the convo")
-      socketsConected.add(socket.id)
-      console.log(socketsConected.size)
-      io.emit("countUser", socketsConected.size);
-    });
-    
-    socket.on("exituser",function(username){
-      socket.broadcast.emit("update", username + " left the convo")
-      socketsConected.delete(socket.id)
-      console.log(socketsConected.size)
-      io.emit("countUser", socketsConected.size);
-    });
-  
-    socket.on('message', (data) => {
-      socket.broadcast.emit('chat-message', data)
-    });
-  
-    socket.on("chat",function(message){
-      socket.broadcast.emit("chat", message);
-    });
-
-    socket.on("typing", function () {
-      socket.broadcast.emit("typing", socket.username);
-    });
-  
-    socket.on("stopTyping", function () {
-      socket.broadcast.emit("stopTyping", socket.username);
-    });
-
-    socket.on("disconnect", function () {
-      if (socket.username) {
-        const username = socket.username;
-        socket.broadcast.emit("update", username + " left the convo");
-        socketsConected.delete(socket.id)
-        console.log(socketsConected.size)
-        io.emit("countUser", socketsConected.size);
-      }
-    });
+io.on("connection", function (socket) {
+  socket.on("newuser", function (username) {
+    socket.username = username;
+    socket.broadcast.emit("update", username + " joined the convo");
+    socketsConected.add(socket.id);
+    io.emit("countUser", socketsConected.size);
   });
-  
-  server.listen(port, () => {
-    console.log(`RJM app listening on port ${port}`)
-  })
 
+  socket.on("message", data => {
+    socket.broadcast.emit("chat-message", data);
+  });
 
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat", message);
+  });
+
+  socket.on("typing", function () {
+    socket.broadcast.emit("typing", socket.username);
+  });
+
+  socket.on("stopTyping", function () {
+    socket.broadcast.emit("stopTyping", socket.username);
+  });
+
+  socket.on("disconnect", function () {
+    if (socket.username) {
+      const username = socket.username;
+      socket.broadcast.emit("update", username + " left the convo");
+      socketsConected.delete(socket.id);
+      io.emit("countUser", socketsConected.size);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`RJM app listening on port ${port}`);
+});
